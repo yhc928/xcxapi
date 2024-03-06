341b138af458e3959f64157f2d6e206ad3868c8e
yhc <yuhaichao928@163.com>
2024-03-07 00:33:31 +0800

    fuwenben

--- app/controller/UploadImg.php
+++ app/controller/UploadImg.php
@@ -11,52 +11,149 @@ class UploadImg extends AdminBase
 {
     public function upload(Request $request){
         $files = $request->file();
-      // $file = Request::file('image');
+        // $file = Request::file('image');
         $info = [];
         //dump($files);
+
         foreach ($files as $file){
 
             $info[] = Filesystem::putfile('topic',$file);
         }
 
-       // dump($info);
+        $ENVID = 'prod-4g7ozm3t0ab77771';
+        $position = strpos($info[0], "20240306");
+        $fileName = substr($info[0],$position+9,strlen($info[0]));
+        $PATH = 'cover/'.$fileName;
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'http://api.weixin.qq.com/tcb/uploadfile',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>'{"env":"'.$ENVID.'","path":"'.$PATH.'"}',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $res = json_decode($response,true);
 
-        if (count($info) != 0){
-            $currentUrl = \think\facade\Request::instance()->domain();
-            $data = ['imageUrl' => '/storage/'.$info[0]];
-            $id = UploadModel::create($data)->getData('id');
-            if (!empty($id)){
+        if ($res['errcode'] == 0){
+            //上传文件到云托管
 
-                $result = [
-                    //状态码
-                    'errno' => 0,
-                    //返回数据
-                    'data' =>['url'=> $currentUrl.'/storage/'.$info[0],'alt'=>'','href'=>'']
-                ];
-                //返回api接口
-                return Response::create($result,'json');
+            $key = $PATH;
+            $Signature = $res['authorization'];
+            $security_token = $res['token'];
+            $meta_fileid =$res['cos_file_id'];
+            $file = file_get_contents(app()->getRootPath().'/public/storage/'.$info[0]);
 
+            $ucurl = curl_init();
+            $postData = array(
+                'key' => $key,
+                'Signature'=> $Signature,
+                'x-cos-security-token' =>$security_token,
+                'x-cos-meta-fileid' =>$meta_fileid,
+                'file' => $file // 上传文件路径
+            );
+            curl_setopt($ucurl, CURLOPT_URL, $res['url']); // 设置URL地址
+            curl_setopt($ucurl, CURLOPT_RETURNTRANSFER, true); // 返回结果而不直接输出到页面
+            curl_setopt($ucurl, CURLOPT_POST, true); // 开启POST请求
+            // 设置POST请求体
+            curl_setopt($ucurl, CURLOPT_POSTFIELDS, $postData);
 
-            }else{
-                $result = [
-                    //状态码
-                    'errno' => 1,
-                    //返回数据
-                    'message' => '失败了'
-                ];
-                //返回api接口
-                return Response::create($result,'json');
-            }
+            $response1 = curl_exec($ucurl);
+            curl_close($ucurl);
+
+        }
+
+        //获取下载链接
+        $param = array(
+            'env' => $ENVID,
+            'file_list' => array(array(
+                'fileid' => $res['file_id'],
+                'max_age' => 86400
+            ))
+        );
+        $curl1 = curl_init();
+        curl_setopt_array($curl1, array(
+            CURLOPT_URL => 'http://api.weixin.qq.com/tcb/batchdownloadfile',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => json_encode($param),
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+        ));
+        $response = curl_exec($curl1);
+        curl_close($curl1);
+        $res = json_decode($response,true);
+        if ($res['errcode'] ==0) {
+            //获取到下载链接
+            $result = [
+                //状态码
+                'errno' => 0,
+                //返回数据
+                'data' =>['url'=> $res['file_list'][0]['download_url'],'alt'=>'','href'=>'']
+            ];
+            //返回api接口
+            return Response::create($result,'json');
         }else{
             $result = [
                 //状态码
                 'errno' => 1,
                 //返回数据
-                'message' => '没有获取到图片'
+                'message' => '失败了'
             ];
             //返回api接口
             return Response::create($result,'json');
         }
+
+//        if (count($info) != 0){
+//            $currentUrl = \think\facade\Request::instance()->domain();
+//            $data = ['imageUrl' => '/storage/'.$info[0]];
+//            $id = UploadModel::create($data)->getData('id');
+//            if (!empty($id)){
+//
+//                $result = [
+//                    //状态码
+//                    'errno' => 0,
+//                    //返回数据
+//                    'data' =>['url'=> $currentUrl.'/storage/'.$info[0],'alt'=>'','href'=>'']
+//                ];
+//                //返回api接口
+//                return Response::create($result,'json');
+//
+//
+//            }else{
+//                $result = [
+//                    //状态码
+//                    'errno' => 1,
+//                    //返回数据
+//                    'message' => '失败了'
+//                ];
+//                //返回api接口
+//                return Response::create($result,'json');
+//            }
+//        }else{
+//            $result = [
+//                //状态码
+//                'errno' => 1,
+//                //返回数据
+//                'message' => '没有获取到图片'
+//            ];
+//            //返回api接口
+//            return Response::create($result,'json');
+//        }
 
     }
 }
